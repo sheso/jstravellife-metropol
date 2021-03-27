4b050dc2257d4b95a4217f1265bccd772697bd1c
AleksandraMakhmutova <honeyms@mail.ru>
2021-03-27 15:36:37 +0300

    bottonNav+style

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -1,11 +1,27 @@
+import { Hidden } from "@material-ui/core";
 import BottomNav from "../BottomNav";
 import style from "./style.module.css";
+import logo from "./logo.png";
 const Main = () => {
   return (
-    <div className={style.conteiner}>
-      Hello from Main
-      <BottomNav />
-    </div>
+    <>
+      <div className={style.conteiner}>
+      <div className={style.imgBox}>
+         <img
+          src={logo}
+          alt=""
+         
+          className={style.image}
+        />
+      </div>
+         
+        
+        
+        <div className={style.navBottom}>
+          <BottomNav />
+        </div>
+      </div>
+    </>
   );
 };
 
