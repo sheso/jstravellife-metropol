3bc3d2a403ca370d4b99708e0215248db9cc6230
AleksandraMakhmutova <honeyms@mail.ru>
2021-03-27 17:01:22 +0300

    button

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-import Main from './components/Main';
-import Services from './components/Services/Services';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Main from "./components/Main";
+import Services from "./components/Services/Services";
+import SpeedDials from "./components/SpeedDials";
 
 function App() {
   return (
     <div className="App">
+      <SpeedDials />
       <Router>
         <Switch>
-          <Route exact path='/'>
+          <Route exact path="/">
             <Main />
           </Route>
-          <Route path='/services'>
+          <Route path="/services">
             <Services />
           </Route>
-          <Redirect to='/' />
+          <Redirect to="/" />
         </Switch>
       </Router>
     </div>
--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -1,11 +1,12 @@
-import { Hidden } from "@material-ui/core";
 import BottomNav from "../BottomNav";
 import style from "./style.module.css";
 import logo from "./logo.png";
+
 const Main = () => {
   return (
     <>
       <div className={style.conteiner}>
+       
         <div className={style.imgBox}>
           <img src={logo} alt="" className={style.image} />
         </div>
