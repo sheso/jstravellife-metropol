f819339baeaec97929f60c7c2c5f300bfc00ac2a
Alexander Blinkov <tingol88@yandex.ru>
2021-03-27 14:11:07 +0300

    React-Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Main from './components/Main';
 import Services from './components/Services/Services';
 
 function App() {
   return (
     <div className="App">
-			<Main />
-      <Services/>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Main />
+          </Route>
+          <Route path='/services'>
+            <Services />
+          </Route>
+          <Redirect to='/' />
+        </Switch>
+      </Router>
     </div>
   );
 }
