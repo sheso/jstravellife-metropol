938be78d47ba3c8c740874324cbfcee617a2ee2e
Alexander Blinkov <tingol88@yandex.ru>
2021-03-27 13:54:17 +0300

    fix redux conflict

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
 import {combineReducers} from 'redux'
 import enReducer from './enReducer'
 import ruReducer from './ruReducer'
 import translateReducer from './translateReducer'
+import userReducer from './userReducer'
 
 const rootReducer = combineReducers({
   ru: ruReducer,
-	en: enReducer,
+  en: enReducer,
+  user: userReducer,
 	translate: translateReducer,
-=======
-import {combineReducers} from 'redux';
-import userReducer from './userReducer';
-
-
-const rootReducer = combineReducers({
-    user: userReducer,
->>>>>>> services
 })
 
 export default rootReducer
