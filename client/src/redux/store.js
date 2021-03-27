9c1baa3c52ea5ea083a73266ea1acd7700390dee
AleksandraMakhmutova <honeyms@mail.ru>
2021-03-27 13:24:24 +0300

    bottomNav

---
+++ client/src/redux/store.js
@@ -0,0 +1,12 @@
+import { createStore } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
+import initState from "./initState"
+import rootReducer from "./reducers/rootReducer"
+
+
+const store = createStore(rootReducer, initState, composeWithDevTools())
+
+
+
+
+export default store
---
+++ client/src/redux/types/languages.js
@@ -0,0 +1,3 @@
+export const ADD_RU = 'ADD_RU';
+export const ADD_EN = 'ADD_EN';
+export const CHANGE_LANG = 'CHANGE_LANG';
