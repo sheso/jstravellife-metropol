ae9f3ae0a46d045d710fde48b6c117a983a63d5b
Alexander Blinkov <tingol88@yandex.ru>
2021-03-27 16:27:10 +0300

    first services

--- client/src/components/Services/Services.js
+++ client/src/components/Services/Services.js
@@ -1,11 +1,18 @@
 import Button from '@material-ui/core/Button';
+import ServiceItem from '../ServiceItem/ServiceItem';
 import { useDispatch, useSelector } from 'react-redux';
+import * as services from './servicesDb.js';
+import style from "./style.module.css";
+
 
 const Services = () => {
   const dispatch = useDispatch();
   // const avia = useSelector(state => state.avia);
-  return (<div>
-    Services
+  return (<div className={style.services_div_wrapper}>
+
+
+    <ServiceItem service={services.hall} />
+    <ServiceItem service={services.wedding} />
     <Button variant="contained" color="primary">
       Hello World
     </Button>
