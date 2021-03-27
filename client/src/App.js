ac9197b504bda4af7f7e1127546ddde4dcd5b2a2
Alexander Blinkov <tingol88@yandex.ru>
2021-03-27 13:47:07 +0300

    Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import Main from './components/Main';
+import Services from './components/Services/Services';
 
 function App() {
   return (
     <div className="App">
 			<Main />
+      <Services/>
     </div>
   );
 }
---
+++ client/src/redux/reducers/userReducer.js
@@ -0,0 +1,26 @@
+import * as TYPES from '../types/types';
+
+// example
+function userReducer(user = {}, action) {
+  switch (action.type) {
+    case TYPES.CHECK_USER_SESSION:
+      return action.data
+    case TYPES.VERIFY_USER:
+      return action.data
+    case TYPES.LOGOUT_USER:
+      return action.data
+      case TYPES.SET_TOURS:
+        return {...user, searchTours: action.data}
+    case TYPES.SET_USERS_TOURS:
+      return {...user, usersTours: action.data}
+    case TYPES.ADD_TOUR:
+      return {...user, usersTours: [...user.usersTours, action.data ]}
+    case TYPES.DELETE_TOUR:
+      return {...user, usersTours: user.usersTours.filter(el => el._id !== action.data)}
+    default:
+      return user;
+  }
+}
+// \example
+
+export default userReducer;
