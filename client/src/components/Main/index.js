ba0eaa3c57973e9e45bb9bc8fdaa4452fba92605
AleksandraMakhmutova <honeyms@mail.ru>
2021-03-28 09:50:21 +0300

    link booking

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -20,12 +20,12 @@ const Main = () => {
             </h5>
           </div>
           <div className={style.booking}>
-            <h5>
+            <a className={style.linkBooking} href="https://be.synxis.com/?_ga=2.208427482.957974354.1616845732-1432913282.1615819046&adult=1&arrive=2021-03-28&chain=21125&child=0&currency=RUB&depart=2021-03-29&hotel=3819&level=hotel&locale=ru-RU&rooms=1">
               {" "}
               {statusLang.translate
                 ? statusLang.en.button[1]
                 : statusLang.ru.button[1]}
-            </h5>
+            </a>
           </div>
         </div>
       </div>
